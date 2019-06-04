408f6c3ea8af9c99584371559fbb56d3b15a487e
RC0032503 <RC0032503@CRO-24-07>
2019-06-04 10:42:59 +0100

    ADDED 3 RESISITORS

--- Assignment 2/Form1.cs
+++ Assignment 2/Form1.cs
@@ -16,5 +16,54 @@ namespace Assignment_2
         {
             InitializeComponent();
         }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double Resistor1, Resistor2, ResistanceT;
+                Resistor1 = Convert.ToDouble(textBox2.Text);
+                Resistor2 = Convert.ToDouble(textBox1.Text);
+                ResistanceT = Resistor1 + Resistor2 ;
+                label3.Text = "Resistance= " + ResistanceT;
+            }
+            catch
+            {
+                MessageBox.Show("Please Type in 2 numbers");
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double Resistor1, Resistor2,Resistor3, ResistanceT;
+                Resistor1 = Convert.ToDouble(textBox4.Text);
+                Resistor2 = Convert.ToDouble(textBox5.Text);
+                Resistor3 = Convert.ToDouble(textBox3.Text);
+                ResistanceT = Resistor1 + Resistor2 + Resistor3;
+                label7.Text = "Resistance= " + ResistanceT;
+            }
+            catch
+            {
+                MessageBox.Show("Please Type in 3 numbers");
+            }
+        }
     }
 }
